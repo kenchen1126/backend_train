3d00c53064957fe0fb9e4acc5092eb844205acad
Ken <kenaser12345@gmail.com>
2021-05-29 16:08:58 +0800

    add index to task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,26 @@
 require 'faker'
-
-100.times do 
+Task.create(name: "吃飯",
+description: Faker::Lorem.paragraph,
+start_at: Faker::Time.backward(days: 2, period: :morning),
+end_at: Faker::Time.forward(days: 4, period: :evening),
+priority: Task.priorities.values.sample,
+status: Task.statuses.values.sample
+)
+Task.create(name: "做作業",
+description: Faker::Lorem.paragraph,
+start_at: Faker::Time.backward(days: 2, period: :morning),
+end_at: Faker::Time.forward(days: 4, period: :evening),
+priority: Task.priorities.values.sample,
+status: Task.statuses.values.sample
+)
+Task.create(name: "玩電腦",
+description: Faker::Lorem.paragraph,
+start_at: Faker::Time.backward(days: 2, period: :morning),
+end_at: Faker::Time.forward(days: 4, period: :evening),
+priority: Task.priorities.values.sample,
+status: Task.statuses.values.sample
+)
+100000.times do 
   Task.create(name: Faker::Lorem.word,
               description: Faker::Lorem.paragraph,
               start_at: Faker::Time.backward(days: 2, period: :morning),
