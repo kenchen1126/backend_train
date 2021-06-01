95f62d6d30b52480b27a93404b97fbecb440d7fe
Ken <kenaser12345@gmail.com>
2021-06-01 12:12:40 +0800

    admin page and user show

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,59 @@
+class Admin::UsersController < ApplicationController
+  before_action :find_user, only: [:show, :create, :update, :destroy]
+
+  def index
+    @users = User.page(params[:page]).per(5)
+    
+  end
+
+  def show
+    @q = @user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true).page(params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = I18n.t("user.create_success")
+      session[:user_id] = @user.id
+      redirect_to admin_users_path
+    else
+      render :new
+      flash[:notice] = I18n.t("user.create_failed")
+    end
+  end
+
+  def update
+    if @user.update(task_params)
+      flash[:notice] = I18n.t("user.edit_success")
+      redirect_to admin_users_path
+    else
+      render :edit 
+      flash[:notice] = I18n.t("user.edit_failed")
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:notice] = I18n.t("user.delete_success")
+    else
+      flash[:notice] = I18n.t("user.delete_failed")
+    end
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+end
